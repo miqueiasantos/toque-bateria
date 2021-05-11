a443c9947a8e195d73e96d090208447dc80bfb6a
Miqueias Santos <falecom@miqueiassantos.com.br>
2021-05-11 12:34:20 -0300

    Update project

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
-  "component---src-pages-content-tsx": () => import("./../../../src/pages/content.tsx" /* webpackChunkName: "component---src-pages-content-tsx" */),
-  "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */)
-}
-
